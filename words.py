3f41ca2d8ae9cb5ed7713328d3fcfa577898fa7f
ibrahim ozgon <ibrahim.ozgon@arabam.com>
2019-01-10 17:36:53 +0300

    new word developments

--- words.py
+++ words.py
@@ -1,15 +1,30 @@
 import flask
 import random
+import urllib
+from bs4 import BeautifulSoup
 
 app = flask.Flask(__name__)
-words = []
+words = None
+
+
+def read_word_count():
+    with open("./files/words.txt", "r+", encoding="utf8")as f:
+        return len(f.readlines())
 
 
 def read_words():
+    with open("./files/words.txt", "r+", encoding="utf8")as f:
+        words = [i.replace("\n", "") for i in f.readlines()
+                 if i and i != "" and i != "\n"]
+        return words
+
+
+def read_random_word():
     global words
-    with open("./files/words.txt", "r+")as f:
-        words = [i.replace("\n", "") for i in f.readlines() if i and i != "" and i != "\n"]
-        random.shuffle(words)
+    if not words:
+        words = read_words()
+    random.shuffle(words)
+    return words[0]
 
 
 @app.route('/')
@@ -19,17 +34,17 @@ def home():
 
 @app.route("/word", methods=["GET"])
 def get_word():
-    index = get_index()
     return flask.jsonify({
-        "word": words[index],
-        "index": index
+        "word": read_random_word(),
+        "index": get_index()
     })
 
 
 @app.route("/finish", methods=["GET"])
 def finish_process():
     index = get_index()
-    rate = int(round(((index + 1) * 100) / len(words)))
+    words_length = read_word_count()
+    rate = int(round(((index + 1) * 100) / words_length))
     result = {
         "rate": rate,
         "result": "You have completed {}% of test!".format(rate),
@@ -42,16 +57,31 @@ def get_index():
     index = flask.request.args.get("index")
     if not index:
         index = 0
-    elif int(index) >= len(words):
-        index = 0
-        random.shuffle(words)
     else:
         index = int(index)
 
     return index
-  
+
+
+@app.route("/learn", methods=["GET"])
+def learn():
+    word = flask.request.args.get("word")
+    url = "http://tureng.com/en/turkish-english/"+urllib.parse.quote_plus(word)
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, "html.parser")
+    table = soup.find('table', attrs={'id': 'englishResultsTable'})
+    rows = table.find_all('tr')
+    data = []
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        if cols and len(cols) > 3:
+            data.append(cols[3])
+
+    return flask.jsonify(data)
+
 
 if __name__ == '__main__':
-    read_words()
+    # learn("book")
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(host='0.0.0.0', port=2442)
